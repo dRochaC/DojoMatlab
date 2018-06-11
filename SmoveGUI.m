b6ca6fc85de1ed51c06d89b91d291e46826c4757
george.sabino <sabinogs@yahoo.com.br>
2018-06-10 21:59:59 -0300

    load arquivo

--- SmoveGUI.m
+++ SmoveGUI.m
@@ -78,4 +78,7 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+dadosDados = load('massa_de_teste.txt');
+
 disp('botão click')
